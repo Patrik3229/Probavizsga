f453a50bf512a125c8be29df05e51c2b4f51da82
Petrik Diak <diak@petrik.hu>
2024-04-16 09:59:29 +0200

    Scema & Member & Payment

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { MemberModule } from './member/member.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [],
+  imports: [MemberModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
